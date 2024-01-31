d4b09ba420a9e01e72198fc1bb9efac15d4744c9
Mbugua <mwaurathealex@gmail.com>
2024-01-31 20:06:46 +0300

    contact form

--- src/app/properties/[propertyId]/page.tsx
+++ src/app/properties/[propertyId]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Button from "~/app/_components/Button";
 import Container from "~/app/_components/Container";
+import Input from "~/app/_components/Inputs/Input";
 import PropertyInfo from "~/app/_components/listings/PropertyInfo";
 import PropertyHead from "~/app/_components/listings/propertyHead";
 import { api } from "~/trpc/react";
@@ -50,6 +52,38 @@ export default function PropertyPage({ params }: { params: IParams }) {
               location={property.location}
               amenities={property.propertyAmenities}
             />
+            <div className="order-first mb-10 md:order-last md:col-span-3">
+              <div className="relative overflow-hidden rounded-xl border border-neutral-200 bg-white">
+                <div className="flex flex-row items-center gap-1 p-4">
+                  <div className="text-xl font-semibold">
+                    KSh {property.price}
+                  </div>
+                  <div className="font-light text-neutral-600">monthly</div>
+                </div>
+                <hr />
+                <div className="flex flex-row items-center gap-1 p-4">
+                  <div className="text-lg font-semibold">{property.type}</div>
+                </div>
+                <hr />
+                <div className="sticky bottom-0">
+                  <div className="flex flex-col gap-5 p-4">
+                    <div className="text-lg font-semibold">
+                      Contact the seller
+                    </div>
+                    <textarea
+                      className="h-1/3 rounded-xl border px-2 py-4
+                        hover:border-2 hover:border-gray-900/30 focus:border-gray-900/40"
+                      rows={8}
+                      placeholder="Leave your message here ..."
+                    />
+                    <Button
+                      label="Send"
+                      onClick={() => console.log("You called>>")}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
